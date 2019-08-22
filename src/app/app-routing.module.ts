bdeea7bfbefe3b0893cbbba270c29a4a019ecae3
Gaurav Batra <Gaurav.Batra@INDECOMM.LOCAL>
2019-08-22 15:06:36 +0530

    changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+// import { AddChecklistTemplateComponent } from './add-checklist-template/add-checklist-template.component';
+// import { ChecklistBuilderComponent } from './checklist-builder/checklist-builder.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+ // { path: '**', pathMatch: 'full', component:  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ChecklistBuilderModule } from './checklist-builder/checklist-builder.module';
-import { AddChecklistTemplateModule } from './add-checklist-template/add-checklist.module';
+// import { ChecklistBuilderModule } from './checklist-builder/checklist-builder.module';
+// import { AddChecklistTemplateModule } from './add-checklist-template/add-checklist.module';
 
-// import { AddChecklistTemplateComponent } from './add-checklist-template/add-checklist-template.component';
-// import { ChecklistBuilderComponent } from './checklist-builder/checklist-builder.component';
+import { AddChecklistTemplateComponent } from './add-checklist-template/add-checklist-template.component';
+import { ChecklistBuilderComponent } from './checklist-builder/checklist-builder.component';
 
 @NgModule({
   declarations: [
-    AppComponent
-    // AddChecklistTemplateComponent,
-    // ChecklistBuilderComponent
+    AppComponent,
+     AddChecklistTemplateComponent,
+     ChecklistBuilderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ChecklistBuilderModule,
-    AddChecklistTemplateModule
+    // ChecklistBuilderModule,
+    // AddChecklistTemplateModule
   ],
   providers: [],
   bootstrap: [AppComponent]
