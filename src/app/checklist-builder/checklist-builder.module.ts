a55adc1653ebb1135911115ec5c32506a44972ad
Gaurav Batra <Gaurav.Batra@INDECOMM.LOCAL>
2019-08-27 11:35:25 +0530

    checklist builder form modified

--- src/app/checklist-builder/checklist-builder.module.ts
+++ src/app/checklist-builder/checklist-builder.module.ts
@@ -5,12 +5,14 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { MatTabsModule } from '@angular/material';
 import { QuestionListComponent } from '../question-list/question-list.component';
 import { MaterialModule } from '.././material-module';
-import {MatFormFieldModule} from '@angular/material/form-field'
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 
 @NgModule({
   declarations: [ChecklistBuilderComponent,
                   QuestionListComponent],
-  imports: [NgbModule, MatTabsModule, MaterialModule, MatFormFieldModule]
+  imports: [NgbModule, MatTabsModule, MaterialModule, MatFormFieldModule, FormsModule, CommonModule, ReactiveFormsModule]
 })
 export class ChecklistBuilderModule {}
